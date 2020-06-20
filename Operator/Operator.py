2a907c1893a448bb241ef13af87fed4b3702c71f
Lewis_Liu <346734535@qq.com>
2020-06-21 04:41:13 +0800

    update Operator file

--- Operator/Operator.py
+++ Operator/Operator.py
@@ -148,6 +148,153 @@ c = a << 2; #240 = 1111 0000
 print ("c的值为：", c )
 
 c = a >> 2; # 15 = 0000 1111
-print ("c的值为：", c ) 
+print ("c的值为：", c )
+
+#Python 逻辑运算符
+'''
+ and 若x为False, x and y 返回False, 否则返回y的计算值。
+ or 若x是非0， 返回x的值，否则返回y的计算值。
+ not 若x为True, 返回False，若为False返回True.
+'''
+a = 10
+b = 20
+ 
+if a and b :
+    print ("变量a和b都为true")
+else :
+    print ("变量a和b有一个不为true")
+    
+if a or b :
+    print ("变量a和b都为true，或其中一个变量为true")
+else :
+    print ("变量a和b都不为true")
+    
+# 修改变量a的值
+a = 0
+if a and b:
+    print ("变量a和b都为true")
+else :
+    print ("变量a和b有一个不为true")
+ 
+if a or b :
+    print ("变量a和b都为true，或其中一个变量为true")
+else :
+    print ("变量a和b都不为true")
+ 
+if not( a and b ) :
+    print ("变量a和b都为false, 或其中一个变量为false")
+else :
+    print ("变量a和b都为true")
 
 
+# Python 成员运算符
+'''
+in 如果在指定的序列中找到值返回True, 否则返回False
+not in 如果在指定的序列中没有找到值返回True, 否则返回False
+'''
+
+a = 10
+b = 20
+list = [1, 2, 3, 4, 5];
+
+if ( a in list ):
+    print ("变量a在给定的列表List中")
+else:
+    print ("变量a不在给定的列表List中")
+
+if ( b not in list ):
+    print ("变量b不在给定的列表List中")
+else:
+    print ("变量b在给定的列表List中")
+
+# 修改变量a的值
+a = 2
+if ( a in list ):
+    print ("变量a在给定的列表List中")
+else:
+    print ("变量a不在给定的列表List中")
+    
+
+# Python 身份运算符
+'''
+is 是判断两个标识符是不是引用自一个对象
+is not 是判断两个标识符是不是引用自不同对象
+'''
+
+a = 20
+b = 20
+
+if ( a is b ):
+    print("a和b有相同的标识")
+else:
+    print("a和b没有相同的标识")
+
+if ( a is not b ):
+    print("a和b没有相同的标识")
+else:
+    print("a和b有相同的标识")
+    
+# 修改变量b的值
+b = 30
+if ( a is b ):
+    print("a和b有相同的标识")
+else:
+    print("a和b没有相同的标识")
+
+if ( a is not b ):
+    print("a和b没有相同的标识")
+else:
+    print("a和b有相同的标识")
+
+'''
+is 与 == 区别：
+is 用于判断两个变量引用对象是否为同一个(同一块内存空间), ==用于判断
+引用变量的值是否相等
+例子：
+>>> a = [1, 2, 3]
+>>> b = a
+>>> b is a 
+True
+>>> b == a
+True
+>>> b = a[:]
+>>> b is a
+False
+>>> b == a
+True
+'''
+# Python 运算符优先级
+'''
+
+**       指数(最高优先级)
+~+-      按位翻转, 一元加号和减号(最后两个的方法名为+@和-@)
+*/%//
++-       加法减法
+>> <<    
+&        位'AND'
+^|
+<= < > >= 比较运算符
+== !=  
+=% =/ =// -= += *= **= 赋值运算符
+is is not 身份运算符 
+in not in 成员运算符   
+not and or 逻辑运算符
+'''
+
+a = 20
+b = 10
+c = 15
+d = 5
+e = 0
+
+e = (a + b) * c / d
+print(e)
+
+e = ((a + b) * c)/ d 
+print(e)
+
+e = (a + b) * (c / d)
+print(e)
+
+e = a + (b * c)/d 
+print(e)

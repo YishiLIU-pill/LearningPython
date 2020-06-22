fc3da7d04b470f329a7349730d8ba68ebbe141c6
Lewis_Liu <346734535@qq.com>
2020-06-23 05:04:42 +0800

    update Tuple.py

--- variable/Tuple.py
+++ variable/Tuple.py
@@ -12,3 +12,23 @@ print (tuple[1:3])        #('like','100+')
 print (tuple[2:])         #('100+','$$$','eateateat')
 print (tinytuple * 2)     #tinytuple_twice
 print (tuple + tinytuple) #打印组合的元组
+
+'''
+>>> tup = (1, 2, 3, 4, 5, 6)
+>>> print(tup[0])
+1
+>>> print(tup[1:5])
+(2, 3, 4, 5)
+>>> tup[0] = 11  # 修改元组元素的操作是非法的
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: 'tuple' object does not support item assignment
+>>>
+'''
+#虽然tuple的元素不可改变，但它可以包含可变的对象，比如list列表
+#构造包含0个或1个元素的元组比较特殊，所以有一些额外的语法规则：
+tup1 = () #空元组
+tup2 = (20,)#一个元素，需要在元素后添加逗号
+print(tup1,tup2)
+
+#String, list和tuple都属于sequence(序列)

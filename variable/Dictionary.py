b945d04fdbe685262e9ae90b920dfa841b2561c7
Lewis_Liu <346734535@qq.com>
2020-06-23 05:59:54 +0800

    change Dictionary.py

--- variable/Dictionary.py
+++ variable/Dictionary.py
@@ -20,7 +20,6 @@ print (tinydict.values()) #输出所有值
 '''
 Python数据类型转换
 int(x[,base])          将x转换为一个整数
-long(x[,base])         将x转换为一个长整数
 float(x)               将x转换到一个浮点数
 complex(real[,imag])   创建一个复数
 str(x)                 将x转换为字符串
@@ -32,7 +31,6 @@ set(s)                 转换为可变集合
 dict(d)                创建一个字典，d必须是一个序列(key,value)元组
 frozenset(s)           转换为不可变集合          
 chr(x)                 将一个整数转换为一个字符
-unichr(x)              将一个整数转换为一个Unicode字符
 ord(x)                 将一个字符转换为它的整数值
 hex(x)                 将一个整数转换为一个十六进制字符串
 oct(x)                 将一个整数转换为一个八进制字符串
